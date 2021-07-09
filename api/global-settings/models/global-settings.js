730ee6c52062edbb247251371982de8344b7159c
Bradley Burgess <bradleyburgess@gmail.com>
2021-07-09 00:49:24 -0400

    Reworked --> Sanctuary Bulletins

--- api/global-settings/models/global-settings.js
+++ api/global-settings/models/global-settings.js
@@ -1,15 +1,8 @@
-"use strict";
-const uuid = require("uuid").v4;
+'use strict';
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {
-  lifecycles: {
-    async beforeCreate(data) {
-      data.bulletin_id = uuid();
-    },
-  },
-};
+module.exports = {};
