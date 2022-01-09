04ade543076752c2f28f6c24ef386cf57186fe45
Bradley Burgess <bradley@pop-os.localdomain>
2022-01-08 21:28:40 -0500

    Update to 3.6.8 & config local postgres

--- config/database.js
+++ config/database.js
@@ -1,15 +1,35 @@
+// Original Quickstart config:
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
+        client: 'postgres',
+        host: env('DB_HOST'),
+        port: env('DB_POST'),
+        database: env('DB_DATABASE'),
+        username: env('DB_USERNAME'),
+        password: env('DB_PASSWORD'),
+        ssl: false
       },
     },
   },
 });
+
